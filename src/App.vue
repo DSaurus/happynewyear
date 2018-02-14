65f8e78cd3a6c43feb9480614c4ff683d36c41e5
Saurus <jia1saurus@gmail.com>
2018-02-14 17:26:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <div class="wrap">
+      <div class="page_00psd"><div class="dv_00psd_layer_0"></div>
+        <div class="group_0"><div class="dv_00psd_layer_1"></div>
+          <div class="dv_00psd_layer_2"></div>
+        </div>
+        <div class="group_1"><div class="dv_00psd_layer_3"></div>
+          <div class="dv_00psd_layer_4"></div>
+          <div class="dv_00psd_layer_5"></div>
+          <div class="dv_00psd_layer_6"></div>
+        </div>
+        <div class="group_2"><div class="dv_00psd_layer_7"></div>
+          <div class="dv_00psd_layer_8"></div>
+          <div class="dv_00psd_layer_9"></div>
+          <div class="dv_00psd_layer_10"></div>
+          <div class="dv_00psd_layer_11"></div>
+        </div>
+        <div class="dv_00psd_layer_12"></div>
+        <div class="group_4"><div class="dv_00psd_layer_46"></div>
+          <div class="dv_00psd_layer_47"></div>
+          <div class="dv_00psd_layer_48"></div>
+          <div class="group_5"><div class="dv_00psd_layer_49"></div>
+            <div class="dv_00psd_layer_50"></div>
+            <div class="dv_00psd_layer_51"></div>
+          </div>
+          <div class="dv_00psd_layer_52"></div>
+          <div class="dv_00psd_layer_53"></div>
+          <div class="dv_00psd_layer_54"></div>
+        </div>
+        <div class="dv_00psd_layer_55"></div>
+        <div class="dv_00psd_layer_56"></div>
+        <div class="dv_00psd_layer_57"></div>
+        <div class="dv_00psd_layer_58"></div>
+        <div class="dv_00psd_layer_59"></div>
+        <div class="dv_00psd_layer_60"></div>
+        <div class="dv_00psd_layer_61"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.page_00psd .dv_00psd_layer_0 { position: absolute; top:50%; width:35.43rem; height:53.15rem; left:0rem; margin-top:-26.575rem; background:url(./images/dv_00psd_layer_0.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_1 { position: absolute; top:50%; width:35.43rem; height:53.15rem; left:0rem; margin-top:-26.575rem; background:url(./images/dv_00psd_layer_1.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_2 { position: absolute; top:50%; width:11.68rem; height:6.28rem; left:23.75rem; margin-top:20.294999999999998rem; background:url(./images/dv_00psd_layer_2.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_3 { position: absolute; top:50%; width:29rem; height:41.25rem; left:3.65rem; margin-top:-19.265rem; background:url(./images/dv_00psd_layer_3.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_4 { position: absolute; top:50%; width:4.72rem; height:3.92rem; left:11.66rem; margin-top:-15.264999999999999rem; background:url(./images/dv_00psd_layer_4.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_5 { position: absolute; top:50%; width:4.26rem; height:8.21rem; left:29.02rem; margin-top:11.754999999999999rem; background:url(./images/dv_00psd_layer_5.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_6 { position: absolute; top:50%; width:2.56rem; height:3.27rem; left:13.77rem; margin-top:21.805000000000003rem; background:url(./images/dv_00psd_layer_6.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_7 { position: absolute; top:50%; width:1.86rem; height:1.85rem; left:1.11rem; margin-top:-15.264999999999999rem; background:url(./images/dv_00psd_layer_7.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_8 { position: absolute; top:50%; width:1.68rem; height:1.66rem; left:2.82rem; margin-top:-18.865rem; background:url(./images/dv_00psd_layer_8.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_9 { position: absolute; top:50%; width:1.73rem; height:1.71rem; left:31.77rem; margin-top:6.595000000000002rem; background:url(./images/dv_00psd_layer_9.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_10 { position: absolute; top:50%; width:1.46rem; height:1.64rem; left:26.36rem; margin-top:-25.224999999999998rem; background:url(./images/dv_00psd_layer_10.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_11 { position: absolute; top:50%; width:21.14rem; height:49.29rem; left:0rem; margin-top:-24.025rem; background:url(./images/dv_00psd_layer_11.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_12 { position: absolute; top:50%; width:7.82rem; height:14.38rem; left:1.11rem; margin-top:-7.835000000000001rem; background:url(./images/dv_00psd_layer_12.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_46 { position: absolute; top:50%; width:6.97rem; height:6.05rem; left:6.32rem; margin-top:13.445000000000004rem; background:url(./images/dv_00psd_layer_46.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_47 { position: absolute; top:50%; width:20.42rem; height:18.14rem; left:8.44rem; margin-top:-10.694999999999999rem; background:url(./images/dv_00psd_layer_47.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_48 { position: absolute; top:50%; width:1.92rem; height:4.35rem; left:14.13rem; margin-top:-2.0249999999999986rem; background:url(./images/dv_00psd_layer_48.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_49 { position: absolute; top:50%; width:13.88rem; height:2.29rem; left:10.54rem; margin-top:6.824999999999999rem; background:url(./images/dv_00psd_layer_49.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_50 { position: absolute; top:50%; width:19.68rem; height:2.75rem; left:7.53rem; margin-top:8.004999999999999rem; background:url(./images/dv_00psd_layer_50.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_51 { position: absolute; top:50%; width:11.74rem; height:2.18rem; left:14.94rem; margin-top:-12.114999999999998rem; background:url(./images/dv_00psd_layer_51.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_52 { position: absolute; top:50%; width:6.28rem; height:7.69rem; left:26.03rem; margin-top:-16.255rem; background:url(./images/dv_00psd_layer_52.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_53 { position: absolute; top:50%; width:14.72rem; height:9.58rem; left:13.51rem; margin-top:12.095000000000002rem; background:url(./images/dv_00psd_layer_53.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_54 { position: absolute; top:50%; width:7.63rem; height:2.06rem; left:18.73rem; margin-top:-14.514999999999999rem; background:url(./images/dv_00psd_layer_54.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_55 { position: absolute; top:50%; width:6.2rem; height:6.56rem; left:27.34rem; margin-top:-7.204999999999998rem; background:url(./images/dv_00psd_layer_55.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_56 { position: absolute; top:50%; width:12.47rem; height:11.92rem; left:0rem; margin-top:14.654999999999998rem; background:url(./images/dv_00psd_layer_56.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_57 { position: absolute; top:50%; width:8.48rem; height:8.98rem; left:9.53rem; margin-top:15.294999999999998rem; background:url(./images/dv_00psd_layer_57.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_58 { position: absolute; top:50%; width:26.16rem; height:18.74rem; left:9.27rem; margin-top:-26.575rem; background:url(./images/dv_00psd_layer_58.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_59 { position: absolute; top:50%; width:7.51rem; height:8.98rem; left:10.37rem; margin-top:-18.615rem; background:url(./images/dv_00psd_layer_59.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_60 { position: absolute; top:50%; width:32.42rem; height:26.61rem; left:0rem; margin-top:-13.285rem; background:url(./images/dv_00psd_layer_60.png); background-size:100% auto; }
+.page_00psd .dv_00psd_layer_61 { position: absolute; top:50%; width:25.26rem; height:20.53rem; left:9.72rem; margin-top:-19.244999999999997rem; background:url(./images/dv_00psd_layer_61.png); background-size:100% auto; }
+
+</style>
